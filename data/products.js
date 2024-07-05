5a8626ac87004eca6778e12d4aca8dcfd9c076b9
Dipanshu Rawat <princerawat2108@gmail.com>
2024-07-05 16:54:06 +0530

    Learn promises and convert code to promises

--- data/products.js
+++ data/products.js
@@ -99,11 +99,10 @@ export function loadProducts(fun) {
       }
       return new Product(productDetails);
     });
-
     console.log('load products');
-
     fun();
   });
+
 
   xhr.open('GET', 'https://supersimplebackend.dev/products');
   xhr.send();
